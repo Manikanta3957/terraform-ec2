d644a90fead0ec71b5e6bf71ff8f5dd530b32623
Vijaya Rekha <99827399+vijayarekha47@users.noreply.github.com>
2024-05-27 12:59:41 +0530

    Update provider.tf

--- ec2-terraform/provider.tf
+++ ec2-terraform/provider.tf
@@ -10,4 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIAXYKJRVJ4VERV6J5D"
+  secret_key = "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"
 }
