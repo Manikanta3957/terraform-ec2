21aa955627e13fd5ba21089177478e4963931872
Manikanta3957 <107246373+Manikanta3957@users.noreply.github.com>
2024-05-28 11:43:00 +0530

    Update provider.tf

--- ec2-terraform/provider.tf
+++ ec2-terraform/provider.tf
@@ -10,6 +10,5 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAXYKJRVJ4VERV6J5D"
-  secret_key = "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"
+
 }
